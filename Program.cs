20af0ddf89c82b10541aa6841e5c61bb2e9b9e3c
Baha <toktobekov1995@gmail.com>
2022-01-08 18:37:19 +0600

    в первые впечатления о c#

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace cs 
+{
+
+    class Program 
+    {
+
+        static void Main ()
+        {
+            Console.Write("Hello World!\n");
+            // sfsfssa
+            /*
+            dassdad
+            asasdas
+            */ 
+            Console.WriteLine("Hello 2");
+            Console.ReadLine();
+        }
+        
+    }
+
+}
