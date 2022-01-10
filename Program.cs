2d8669e92f3dc23251018e627a1216645c021d57
Baha <toktobekov1995@gmail.com>
2022-01-10 17:19:58 +0600

    работа с типы данными

--- Program.cs
+++ Program.cs
@@ -1,23 +1,40 @@
 ﻿using System;
 
-namespace cs 
+namespace cs
 {
+    // class Program
+    // {
+    //     static void Main(string[] args)
+    //     {
+    //         // byte number = 255; "8 бит"
+    //         // short number = 15000; "16 бит"
+    //         // int number = 5251; // " 32 бит"
+    //         // long number = NewMethod(); "64 бит"
 
-    class Program 
+    //         float number = 15000.05f;
+
+    //         // double number = 5.15d;
+    //         string word = "Переменная: ";
+    //         // char symbol = 'sFsa';
+
+    //         // bool is_happy = false;
+
+    //         Console.WriteLine(word + number + "!");
+
+    //     }
+
+    // }
+    class IntegerMethod
     {
-
-        static void Main ()
+        static void Main()
         {
-            Console.Write("Hello World!\n");
-            // sfsfssa
-            /*
-            dassdad
-            asasdas
-            */ 
-            Console.WriteLine("Hello 2");
-            Console.ReadLine();
+            int num_1 = 0, num_2 = 0;
+
+            num_1 = Convert.ToInt32(Console.ReadLine());
+            num_2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("First: " + num_1 + ". Second: " + num_2);
         }
-        
     }
 
 }
